a014aaa5fcb8a071fcfde0d5040d740860f6728d
Sneha Tripathi <st0744397@gmail.com>
2026-03-09 17:29:56 +0530

    for off loop

---
+++ 01_basic/forOf.js
@@ -0,0 +1,28 @@
+// for off array or maps me use hota hai object me nhi
+
+let array=[1,2,3,4,5]
+for (const ele of array) {
+    console.log(ele*2);
+    
+}
+
+// maps two types of maps used in js
+// this map used like object and holds key and pair values
+
+const map=new Map()
+
+//set map
+
+map.set("name","saniya")
+map.set("age",20)
+
+//get map
+map.get("age")
+console.log(map.get("age"));
+
+//for off loop in map
+
+for (const [key,value] of map) {
+    console.log(key,value);
+    
+}
