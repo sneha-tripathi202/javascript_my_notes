077b2f5ba7025c88717af17efe336f454a85f149
Sneha Tripathi <st0744397@gmail.com>
2026-03-12 23:54:04 +0530

    callback

--- 04_callback_and_promises/callback.js
+++ 04_callback_and_promises/callback.js
@@ -1,29 +1,51 @@
 ///first way  of callback
 
-function add(a,b, callback){
-  let sum= a+b;
-  callback(sum);
-}
+// function add(a,b, callback){
+//   let sum= a+b;
+//   callback(sum);
+// }
 
-let callback =(sum)=>{
-    console.log("two no sum",sum);
+// let callback =(sum)=>{
+//     console.log("two no sum",sum);
     
-}
-add(4,5,callback);
+// }
+// add(4,5,callback);
 
-//second way of callback 
+// //second way of callback 
 
-let printName=(name,callback)=>{
-  callback(name);
-  console.log(name);
+// let printName=(name,callback)=>{
+//   callback(name);
+//   console.log(name);
+  
+// }
+
+// printName ("sneha",(n)=>{
+    // let para= document.createElement('h1').innerText=n;
+    // let h= document.querySelector("h1");
+    // h.append(para);
+
+    
+
+// })
+
+function add (num1,num2,callback)
+{
+  let add=num1+num2
+  callback(add)
+} 
+
+let callback=(add)=>{
+  console.log("adiiton of two no",add);
   
 }
+add(2,3,callback)
 
-printName ("sneha",(n)=>{
-    let para= document.createElement('h1').innerText=n;
-    let h= document.querySelector("h1");
-    h.append(para);
+//question 
 
-    
+ function printme(params) {
+  console.log("sneha");
+  
+}
+const printsneha=setTimeout(printme,2000)
 
-})
+// question
--- 04_callback_and_promises/tempCodeRunnerFile.js
+++ 04_callback_and_promises/tempCodeRunnerFile.js
@@ -1,10 +1,1 @@
-onsole.log('promise6 ');
-// fetch('https://api.github.com/users/hiteshchoudhary')
-// .then((response)=>{
-//     return response.json()
-// }).then((data)=>
-// {   console.log('withthennnnnnnnnnnnnnnn.......');
-
-//     console.log(data)
-// }).catch((error)=>{console.log(error);
-// })
+})
