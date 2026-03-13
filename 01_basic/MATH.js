6d874f41a7bf68da1c0f4189709a9ac3b3af277e
Sneha Tripathi <st0744397@gmail.com>
2026-03-13 16:52:59 +0530

    math methods

---
+++ 01_basic/MATH.js
@@ -0,0 +1,24 @@
+//math methods
+
+console.log(Math.abs(-4));//change sign
+console.log(Math.round(4.5));//for absolute value 5
+console.log(Math.ceil(4.3));//upper 4
+console.log(Math.floor(4.8));//4 lower
+console.log(Math.random());//generates random valve
+console.log(Math.max(2,3,45,5));//54
+console.log(Math.min(2,3,45,5));//2
+
+console.log(Math.random()*100)//ye 0<=x<=1; 100 se
+//  multiple karte hai to 1 se 100 maximum hoti hai kbhi 1 uske liye 1
+//  se add karna pdta hai
+//  nhi deta normally yhan 100 nhi dega 
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+  //(0.76*11)=8.4+10=18.4=18
+
+
+
+
