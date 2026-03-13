7fb71e3e998cb578de0d70cda6f029978d226299
Sneha Tripathi <st0744397@gmail.com>
2026-03-13 12:07:59 +0530

    array

--- 01_basic/array.js
+++ 01_basic/array.js
@@ -1,36 +1,36 @@
-// forofloop used to iterate over iterable objects like arrays,strings,maps,sets etc
-const array=[1,3,4,5,6]
+let array= [1,2,3,4]
+console.log(typeof array.toString());//1,2,3,4
+console.log(array.length);//4
+console.log(array.at(0));//1
 
-for(const i of array)
-{
-    console.log(i);
-}
+let array2=[5,6,7,8,9]
+console.log(array2.join("@"));//form of string 5@6@7@8@9
+array2.push(3)
+console.log(array2);
+array2.pop()
+console.log(array2);
+array2.shift()
+console.log("s",array2);
+array2.unshift(9)
+console.log(array2);
 
- const map=new Map();
-   map.set(1,"sneha");
-   map.set(2,"aasha");
-   map.set(3,"swati");
-    console.log(map);
-    console.log(map.get(2));
+array2.includes("9")
+console.log(array2);
+console.log(Array.isArray(array));
+let array3=array2.concat(array)
+console.log(array3);
 
-    for(const[key,value]of map){
-        console.log(key,value);
-        
-    }
+//allmethods returns array
+
+let array4=["sneha","ram","syam","lalit", "radha"]
+console.log(array4.slice(1,2));//start and end index ["ram","sym","lalit"]
+//Returns a copy of a section of an array
+console.log(array4.splice("sneha",3))//["sneha","ram","syam"]
+//Removes and adding elements from an array and, if necessary,
+//  inserts new elements in their place, returning the deleted elements.
+console.log(array4.splice(0,2,"sita"))//["lalit", "radha"]
+console.log(array4);//["sita"]
 
 
-    // for-in loops using objects and arrays
-const obj={
-    name:"sneha",
-    age:25,
-    belongsto:"India"
-}
-for(const key in obj){
-    console.log(`${key} value is ${obj[key]} `);
-    
-}
-  for(const i in array){
-      console.log(`Index ${i} has value ${array[i]}`);
-  }
-  
-    
+
+
