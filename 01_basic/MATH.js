ee98eff28b6964ccd4bd241e90b98986d9622570
Sneha Tripathi <st0744397@gmail.com>
2026-03-13 16:58:38 +0530

    math methods

--- 01_basic/MATH.js
+++ 01_basic/MATH.js
@@ -19,6 +19,13 @@ const max=20
 console.log(Math.floor(Math.random()*(max-min+1)+min));
   //(0.76*11)=8.4+10=18.4=18
 
+  //tofixed (decimal ke baad jitne no chahiye hote hai)
+
+  let a=2.3456
+  console.log(a.toFixed(3));
+  
+  
+
 
 
 
