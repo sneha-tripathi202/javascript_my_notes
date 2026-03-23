5fe4411e8630ea577d388ef8b1147ae8d1aeb6ff
Sneha Tripathi <st0744397@gmail.com>
2026-03-23 23:13:41 +0530

    practice questiion

--- 01_basic/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-let totalitem=pensinfo2.map((item,i)=>{
-//     return item
-// }).reduce((acc,item)=>acc+item.price,0)///jo array map se aayega useko fir ye proccess karata hai
-//        console.log(totalitem);
