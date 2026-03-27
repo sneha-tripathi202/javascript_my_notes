b73840f600f6780afd05d6cd9a34a9c65d13d246
Sneha Tripathi <st0744397@gmail.com>
2026-03-27 22:54:05 +0530

    promises

--- 04_callback_and_promises/promise_practice.js
+++ 04_callback_and_promises/promise_practice.js
@@ -73,67 +73,89 @@ test();
 
 
 
-// const clickme = new Promise((resolve, reject) => {
+const clickme = new Promise((resolve, reject) => {
 
-//     setTimeout(() => {
-//         var i = false;
-//         if (i == true) {
-//             console.log("promise one .........");
-//             resolve({
-//                 name: "sneha",
-//                 age: 21
-//             });
-//         }
-//         else {
-//             reject();
-//         }
-//     }, 2000);
-// })
+    setTimeout(() => {
+        var i = false;
+        if (i == true) {
+            console.log("promise one .........");
+            resolve({
+                name: "sneha",
+                age: 21
+            });
+        }
+        else {
+            reject();
+        }
+    }, 2000);
+})
 
 
-// ///promise cosumtion
-// clickme.then((personalinfo) => {
-//     console.log(personalinfo);
-//     return personalinfo.name;
+///promise cosumtion
+clickme.then((personalinfo) => {
+    console.log(personalinfo);
+    return personalinfo.name;
 
-// }).then((name) => {
-//     console.log(name);
+}).then((name) => {
+    console.log(name);
 
-// }).catch((err) => {
-//     console.log("error");
+}).catch((err) => {
+    console.log("error");
 
-// }).finally(() => {
-//     console.log("promise is completed");
+}).finally(() => {
+    console.log("promise is completed");
 
-// })
+})
 
-// const click = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         var i = false;
-//         if (i == true) {
-//             console.log("promise one .........");
-//             resolve({
-//                 name: "sneha",
-//                 age: 21
-//             });
-//         }
-//         else {
-//             reject();
-//         }
-//     }, 2000);
-// })
+const click = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        var i = false;
+        if (i == true) {
+            console.log("promise one .........");
+            resolve({
+                name: "sneha",
+                age: 21
+            });
+        }
+        else {
+            reject();
+        }
+    }, 2000);
+})
 
-// console.log("async funtion");
+console.log("async funtion");
 
-// async function changeme() {
-//     try {
-//         let promise = await click
-//         console.log(promise);
+async function changeme() {
+    try {
+        let promise = await click
+        console.log(promise);
 
-//     } catch (error) {
-//         console.log("Error in asyc");
+    } catch (error) {
+        console.log("Error in asyc");
 
-//     }
-// }
-// changeme();
+    }
+}
+changeme();
+
+///promise
+
+//promise creation 
+
+const promisepra=new Promise((rej,res)=>{
+  let name="sneha"
+  if(name=="sneha"){
+    setTimeout(()=>{
+      console.log('hello sneha');
+      res()
+    },2000)
+  }else{
+    rej()
+  }
+}).then(()=>{
+  console.log(" completed");
+  
+ }).catch(()=>{
+  console.log("not complete");
+  
+})
 
---
+++ 04_callback_and_promises/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+ .catch(()=>{
+  console.log("not complete");
+  
+})
+
