50ce826be23b470409e1c0120a7066f892927602
Sneha Tripathi <st0744397@gmail.com>
2026-03-31 11:34:48 +0530

    module

---
+++ 01_basic/module.js
@@ -0,0 +1,3 @@
+// "Modules in JavaScript allow us to split code into separate files.
+//  We use export to expose code and import to use it elsewhere. This
+//  improves reusability, maintainability, and makes projects scalable."
